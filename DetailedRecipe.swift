c6d005e1a3ba55618dd42dd345c367c369a6ae1a
jva24 <112199589+jva24@users.noreply.github.com>
2023-12-28 01:23:36 -0600

    Add files via upload
    An IOS mobile recipe fetch app coded in SwiftUI that shows the user a variety of dessert recipes available in TheMealDB database. [Fetch Coding Assignment]

---
+++ DetailedRecipe.swift
@@ -0,0 +1,13 @@
+//
+//  DetailedRecipe.swift
+//  RecipeFetchOS
+//
+//  Created by Meto Ajagu on 12/14/23.
+//
+//
+//import Foundation
+//import SwiftUI
+//import SDWebImageSwiftUI
+//
+//struct DetailedRecipe: View {
+//}
---
+++ RecipeFetchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFetchOSApp.swift
+//  RecipeFetchOS
+//
+//  Created by Meto Ajagu on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFetchOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipeHome()
+        }
+    }
+}
